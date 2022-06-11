8d579e538703d14b3e246e509ac515bf6ffe4d0a
Javier Gomez <javiergomezve@gmail.com>
2022-06-11 18:52:13 -0500

    [ADD] http request config

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exercisesOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_EXERCISE_DB_KEY,
+        'X-RapidAPI-Host': process.env.REACT_APP_EXERCISE_DB_HOST,
+    },
+};
+
+export const fetchData = async (url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+};
